a800ff44ccf16a887399bc20efa6981c10f07a93
Himanshu Singhal <himanshusinghal11082004@gmail.com>
2024-09-26 14:10:05 +0530

    sortcoclor

--- src/Assignment_6685/LinearSort.java
+++ src/Assignment_6685/LinearSort.java
@@ -10,8 +10,31 @@ public class LinearSort {
         for (int i = 0; i < arr.length; i++) {
             arr[i] = sc.nextInt();
         }
-//        for (int i = 0; i < n; i++) {
-//            if
-//        }
+
+        int low = 0;
+        int mid = 0;
+        int high = n-1;
+        while(mid<=high){
+            if(arr[mid]==0){
+                swap(arr,low,mid);
+                low++;
+                mid++;
+            } else if (arr[mid]==1) {
+                mid++;
+            } else{
+                swap(arr,high,mid);
+                high--;
+
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+
+    public static void swap(int[] arr,int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
