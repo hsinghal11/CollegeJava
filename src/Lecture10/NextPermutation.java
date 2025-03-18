e1780bf80ba714dd2181210ec0249bdf92c8a9bc
Your Name <youremail@yourdomain.com>
2025-03-18 12:54:22 +0530

    Recursion+backtrack_start

--- src/Lecture10/NextPermutation.java
+++ src/Lecture10/NextPermutation.java
@@ -4,27 +4,27 @@ import java.util.Arrays;
 
 public class NextPermutation {
     public static void main(String[] args) {
-        int[] arr ={3,2,1};
+        int[] arr = {3, 2, 1};
         nextPer(arr);
         System.out.println(Arrays.toString(arr));
     }
 
-    public static void nextPer(int[] arr){
-        int p =-1;
+    public static void nextPer(int[] arr) {
+        int p = -1;
         int n = arr.length;
-        for (int i = n-2; i >= 0; i--) {
-            if(arr[i]<arr[i+1]){
+        for (int i = n - 2; i >= 0; i--) {
+            if (arr[i] < arr[i + 1]) {
                 p = i;
                 break;
             }
         }
-        if(p==-1){
-            Reverse(arr,0,n-1);
+        if (p == -1) {
+            Reverse(arr, 0, n - 1);
             return;
         }
-        int q =-1;
-        for (int i = n-1; i > p ; i--) {
-            if(arr[i] > arr[p]){
+        int q = -1;
+        for (int i = n - 1; i > p; i--) {
+            if (arr[i] > arr[p]) {
                 q = i;
                 break;
             }
@@ -34,7 +34,7 @@ public class NextPermutation {
         arr[p] = arr[q];
         arr[q] = temp;
 
-        Reverse(arr,p+1,n-1);
+        Reverse(arr, p + 1, n - 1);
 
     }
 
